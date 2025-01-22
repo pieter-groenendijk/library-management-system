c7c5f57ca0512f2504fb1b114960c3f03cdeaf84
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-01-22 20:55:06 +0100

    WIP: Notficiation repository

--- application/web-ui/src/main/java/com/github/pieter_groenendijk/controller/NotificationsController.java
+++ application/web-ui/src/main/java/com/github/pieter_groenendijk/controller/NotificationsController.java
@@ -1,16 +1,30 @@
 package com.github.pieter_groenendijk.controller;
 
+import com.github.pieter_groenendijk.repository.notifications.NotificationsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NotificationsController {
+    private final NotificationsRepository REPOSITORY;
+
+    public NotificationsController(
+        NotificationsRepository repository
+    ) {
+        this.REPOSITORY = repository;
+    }
+
     @GetMapping("/notifications")
     public String render(Model model) {
         model.addAttribute(
             "title",
             "Notifications"
+        );
+
+        model.addAttribute(
+            "notification",
+            this.REPOSITORY.retrieve(1L).orElseThrow()
         );
 
         return "notifications/index";
---
+++ application/web-ui/src/main/java/com/github/pieter_groenendijk/repository/notifications/NotificationsRepository.java
@@ -0,0 +1,24 @@
+package com.github.pieter_groenendijk.repository.notifications;
+
+import com.github.pieter_groenendijk.model.Notification;
+import com.github.pieter_groenendijk.repository.Repository;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@org.springframework.stereotype.Repository
+public class NotificationsRepository extends Repository {
+    private final String BASE_PATH = "/notifications";
+
+    public NotificationsRepository(RestTemplate restTemplate) {
+        super(restTemplate);
+    }
+
+    public Optional<Notification> retrieve(Long notificationId) {
+        return super.retrieve(
+            BASE_PATH + "/{notificationId}",
+            Notification.class,
+            notificationId
+        );
+    }
+}
