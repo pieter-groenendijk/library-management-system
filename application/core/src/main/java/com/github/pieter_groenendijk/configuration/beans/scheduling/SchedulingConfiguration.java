e3cd61ca7eb3eb5cc0f2c7779407dbfc53edef1b
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-01-24 13:08:16 +0100

    Removed commented out code from configuration

--- application/core/src/main/java/com/github/pieter_groenendijk/configuration/beans/scheduling/SchedulingConfiguration.java
+++ application/core/src/main/java/com/github/pieter_groenendijk/configuration/beans/scheduling/SchedulingConfiguration.java
@@ -1,10 +1,6 @@
 package com.github.pieter_groenendijk.configuration.beans.scheduling;
 
-import com.github.pieter_groenendijk.entity.event.Event;
-import com.github.pieter_groenendijk.repository.event.EventRepository;
-import com.github.pieter_groenendijk.repository.scheduling.ITaskRepository;
 import com.github.pieter_groenendijk.scheduling.TaskScheduler;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +10,4 @@ public class SchedulingConfiguration {
     public TaskScheduler taskScheduler() {
         return new TaskScheduler(2);
     }
-
-//    @Bean
-//    public ITaskRepository<Event<?>> eventTaskRepository(
-//        SessionFactory sessionFactory
-//    ) {
-//        return (ITaskRepository<Event<?>>) new EventRepository(sessionFactory);
-//    }
 }
